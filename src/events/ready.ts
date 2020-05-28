52ef3b8186d6a3e8591ba510858ccea40360f85d
notfood <notfooddev@outlook.com>
2020-05-29 00:08:07 +0200

    Add check if server settings exist for guild on ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,29 @@
 import { Client as DiscordClient } from "discord.js";
 import Logger from "../lib/log";
+import ServerSettingsRepository from "../repository/severSettings";
 
 export default async function ReadyEvent(discordClient: DiscordClient) {
-	Logger.info(`Ready to serve`);
+	Logger.info(`Ready to serve, found ${discordClient.guilds.cache.size} guilds`);
 
-	// TODO:
-	// Check for any added guilds while offline to create settings objects for
+	discordClient.guilds.cache.forEach(guild => {
+		Logger.info(`Checking for guild ${guild.id}`);
+		ServerSettingsRepository.GetByGuildId(guild.id).then(serverSettings => {
+			if (serverSettings === null) {
+				ServerSettingsRepository.Save({
+					id: 0,
+					guildId: guild.id,
+					deleted: null,
+					prefix: ';',
+					logChannel: null,
+					modLogChannel: null,
+					systemNotice: true,
+					streamLiveRole: null,
+					streamShout: null,
+					adminRole: null, 
+					moderatorRole: null,
+				});
+				Logger.info(`Created guild ${guild.id}`);
+			}
+		});
+	});
 }
