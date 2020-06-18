706fd72c2b873fd2ec00dd3c12f174ec255c694c
notfood <notfooddev@outlook.com>
2020-06-18 23:33:59 +0200

    Added 6 hour timeout for repeating stream promotion

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -1,6 +1,7 @@
 import { Client as DiscordClient, Presence } from "discord.js";
 import ServerSettingsRepository from "../repository/severSettings";
 import WhiteListedGamesRepository from "../repository/whiteListedGames";
+import StreamBuffer from "../repository/streamBuffer";
 import Logger from "../lib/log";
 import TwitchClient from "../lib/twitch";
 import { getTextChannel } from "../lib/util";
@@ -25,7 +26,22 @@ export default async function PresenceUpdateEvent(discordClient: DiscordClient, 
 		return;
 	}
 
-	const whiteListed = await CheckGameWhitelisted(streamingActivity);
+  const whiteListed = await CheckGameWhitelisted(streamingActivity);
+  
+  if (serverSettings.streamLiveRole !== null || serverSettings.streamShout !== null) {
+    const sb = StreamBuffer.getInstance()
+    let timeout = sb.get(guildMember.user.id)
+
+    if (streamingActivity !== undefined) {
+      if (timeout !== null && timeout < new Date()) {
+        return;
+      } else {
+        timeout = new Date()
+        timeout.setTime(timeout.getTime() + (6*60*60*1000))
+        sb.set(guildMember.user.id, timeout)
+      }
+    }
+  }
 
 	if (serverSettings.streamLiveRole !== null) {
 		const liverole = await guild.roles.fetch(serverSettings.streamLiveRole);
