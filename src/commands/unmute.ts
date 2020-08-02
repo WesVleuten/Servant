e07b054f4d4a46d28b7ce3e872b237d4187ae9a2
notfood <notfooddev@outlook.com>
2020-08-02 21:20:37 +0200

    Fix

--- src/commands/unmute.ts
+++ src/commands/unmute.ts
@@ -4,42 +4,42 @@ import MutedRepository from "../repository/muted";
 import ServerSettingsRepository from "../repository/severSettings";
 
 export default class UnmuteCommand implements ICommand {
-  
+	
 	commandName = 'unmute';
 	aliases = null;
 	permissionLevel = PermissionLevel.Moderator;
 	guildOnly = false;
 
 	usageText = ";unmute <user>";
-  helpText = "Unmutes user";
-  
-  async run(discordClient: Client, message: Message, args: string[]) {
-    if (args.length !== 1 || message.mentions.members === null || message.mentions.members.size === 0) { 
-      return;
-    }
+	helpText = "Unmutes user";
+	
+	async run(discordClient: Client, message: Message, args: string[]) {
+		if (args.length !== 1 || message.mentions.members === null || message.mentions.members.size === 0) { 
+			return;
+		}
 
-    const guildId = message.guild?.id;
-    const serverSettings = await ServerSettingsRepository.GetByGuildId(guildId);
-    if (serverSettings === null || serverSettings.muteRole === null) {
-      return;
-    }
+		const guildId = message.guild?.id;
+		const serverSettings = await ServerSettingsRepository.GetByGuildId(guildId);
+		if (serverSettings === null || serverSettings.muteRole === null) {
+			return;
+		}
 
-    const muteRole = await message.guild?.roles.fetch(serverSettings.muteRole);
-    if (!muteRole) {
-      return;
-    }
+		const muteRole = await message.guild?.roles.fetch(serverSettings.muteRole);
+		if (!muteRole) {
+			return;
+		}
 
-    const guildMember = message.mentions.members.first()
-    if (!guildMember || !args[0].includes(guildMember!.id)) { 
-      return;
-    }
+		const guildMember = message.mentions.members.first()
+		if (!guildMember || !args[0].includes(guildMember!.id)) { 
+			return;
+		}
 
-    MutedRepository.Remove(guildId, guildMember.id)
-    const user = message.guild!.members.resolve(guildMember.id);
-    if (user === null) { 
-      return;
-    }
-  
-    user.roles.remove(muteRole, "Mute manually removed via command")
-  }
+		MutedRepository.Remove(guildId, guildMember.id)
+		const user = message.guild!.members.resolve(guildMember.id);
+		if (user === null) { 
+			return;
+		}
+	
+		user.roles.remove(muteRole, "Mute manually removed via command")
+	}
 }
