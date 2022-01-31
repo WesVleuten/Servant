132358f3e071b729d590b6d5ebd29a61d0098ff2
notfooddev <notfooddev@outlook.com>
2022-01-31 18:33:50 +0100

    Improved quotes functionality

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -11,11 +11,12 @@ export default async function MessageReactionAddEvent(discordClient: DiscordClie
     return;
   }
 
+  const reactionsCount = messageReaction.users.cache.filter(u => u.id !== messageReaction.message.author.id).size + 1;
   if (messageReaction.count === null
     || serverSettings.quoteChannel === null
     || serverSettings.quoteEmoji === null
     || messageReaction.message.author.bot
-    || messageReaction.users.cache.filter(u => u.id !== messageReaction.message.author.id).size < serverSettings.quoteThreshold
+    || reactionsCount < serverSettings.quoteThreshold
     || Buffer.from(`${messageReaction.emoji.name}`).toString('base64') !== serverSettings.quoteEmoji) {
     return;
   }
@@ -25,34 +26,41 @@ export default async function MessageReactionAddEvent(discordClient: DiscordClie
     return;
   }
 
+  const msg = messageReaction.message;
+  const emoji = Buffer.from(serverSettings.quoteEmoji, 'base64');
+  const content = `${reactionsCount} ${emoji} **${msg.member.toString()} in ${msg.channel.toString()}**`;
+
+  const embed = createMessageEmbed({
+    color: 0xFFA500,
+    footer: `React with ${emoji} to get a worthy message highlighted!`,
+    thumbnail: msg.author.avatarURL()
+  });
+  let files = null;
+
+  if (msg.attachments.size == 1 && msg.attachments.every(x => x.url.endsWith("png") || x.url.endsWith("jpeg") || x.url.endsWith("jpg") || x.url.endsWith("gif"))) {
+    embed.setImage(msg.attachments.first().url)
+  } else {
+    files = msg.attachments.array();
+  }
+
+  if (msg.content != "") {
+    embed.addField(`**${msg.member.nickname || msg.author.username} said**`, msg.content);
+  }
+
+  embed.addField(`Source`, `[Click](${msg.url})`);
+
+  const quoteMsg = {
+    content: content,
+    files: msg.attachments.array(),
+    embed: embed
+  };
+
   const quote = await QuotesRepository.Get(guildId!, messageReaction.message.id);
   if (!quote) {
-    const msg = messageReaction.message;
-
-    const emoji = Buffer.from(serverSettings.quoteEmoji, 'base64');
-    let content = `${emoji} **${msg.member.toString()} in ${msg.channel.toString()}**`;
-    const embed = createMessageEmbed({
-      color: 0xFFA500,
-      footer: `React with ${emoji} to get your message highlighted!`
-    });
-    let files = null;
-
-    if (msg.attachments.size == 1 && msg.attachments.every(x => x.url.endsWith("png") || x.url.endsWith("jpeg") || x.url.endsWith("jpg") || x.url.endsWith("gif"))) {
-      embed.setImage(msg.attachments.first().url)
-    } else {
-      files = msg.attachments.array();
-    }
-
-    if (msg.content != "") {
-      embed.addField(`**${msg.member.nickname || msg.author.username} said**`, msg.content);
-    }
-
-    embed.addField(`Source`, `[Click](${msg.url})`);
-    const botMessage = await channel.send({
-      content: content,
-      files: msg.attachments.array(),
-      embed: embed
-    });
+    const botMessage = await channel.send(quoteMsg);
     QuotesRepository.Add(guildId, botMessage.id, msg.id)
+  } else {
+    let botMessage = await channel.messages.fetch(quote.botMessageId);
+    await botMessage.edit(quoteMsg);
   }
 }
